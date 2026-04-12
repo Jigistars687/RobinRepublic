9baaaf3a48011ec85cb2e82b026214f09a4b8f92
Jigistar <bebrator123h@gmail.com>
2026-04-12 10:31:46 +0300

    configs

---
+++ Assets/!Game/Scripts/Configs/BuildingConfig.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Building Config")]
+public class BuildingConfig : ScriptableObject
+{
+    public int Price;
+    public GameObject Prefab;
+    public GameObject PreviewPrefab;
+}
---
+++ Assets/!Game/Scripts/Configs/FieldConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Field Config")]
+public class FieldConfig : ScriptableObject
+{
+    public float CellSize;
+}
